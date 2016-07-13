e59fa037a65d87481ebfb90f04cad31f952c3b98
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-13 11:00:57 -0400

    Fix student

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20160713133732) do
+ActiveRecord::Schema.define(version: 20160713134534) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "name"
