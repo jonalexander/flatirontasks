1b7d208438bb49db0b511bba7a26ecb75c7a82e8
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-13 12:52:28 -0400

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+jon = Student.new(name: 'jon', email: 'jon@flatiron-school.com', password: '12345')
+clark = Student.new(name: 'clark', email: 'clark@flatiron-school.com', password: '12345')
+jon = Student.new(name: 'laurie', email: 'laurie@flatiron-school.com', password: '12345')
+
+
+jeff = Instructor.new(name: 'jeff', email: 'jeff@flatiron-school.com', password: '666666')
+sam = Instructor.new(name: 'sam', email: 'sam@flatiron-school.com', password: '123123')
+leigh = Insturctor.new(name: 'leigh', email: 'leigh@flatiron-school.com', password: '667788')
+avi = Instructor.new(name: 'avi', email: 'avi@flatiron-school.com', password: '888888')
+steven = Instructor.new(name: 'steven', email: 'steve@flatiron-school.com', password: '454545')
+
+
+pets = Cohort.new(name:'pets')
+bangarangs = Cohort.new(name: 'bangarangs')
+ios = Cohort.new(name: 'iOS')
+
+
+rails_lab = Assignment.new(name: 'Rails Lab', time: 3)
+sinatra_lab = Assignment.new(name: 'Sinatra Lab', time: 2)
+mvc_lab = Assignment.new(name: 'MVC Lab', time: 5)
+group_rails_project = Assignment.new(name: 'Group Project - Rails', time: 6)
+group_js_project = Assignment.new(name: 'Group Project - JS', time: 3)
+
+
+research_blog = Task.new(name: 'Research my blog post', description: 'Some ideas for my blog post..........', time: 2)
+side_project = Task.new(name: 'Finish side project', description: 'List of incomplete tasks.....', time: 1)
+lunch = Task.new(name: 'Lunch', description: 'Meet friend for lunch', time: 1)
+
