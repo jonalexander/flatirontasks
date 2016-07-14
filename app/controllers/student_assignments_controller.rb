890579b095064164cdc04fa206903f5c5319c668
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-14 15:34:22 -0400

    Add complete button method

--- app/controllers/student_assignments_controller.rb
+++ app/controllers/student_assignments_controller.rb
@@ -1,2 +1,10 @@
 class StudentAssignmentsController < ApplicationController
+    def complete  
+    @student = Student.find(params[:id])
+    @assignment = Assignment.find(params[:assignment][:assignment_id])
+    @student_assignment = StudentAssignment.where(student_id: @student.id).where(assignment_id: @assignment.id)
+    @student_assignment.first.status = true
+    @student_assignment.first.save
+    redirect_to student_path(@student)
+  end
 end
