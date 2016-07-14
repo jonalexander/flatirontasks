d33673090af014d666d0c5c58badf2e125d55ae2
supermancbs <clark.sacktor@gmail.com>
2016-07-14 10:06:35 -0400

    add methods

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -13,7 +13,12 @@ class Instructor < ApplicationRecord
     Student.joins(cohort: :instructors).where("instructors.id = ?", self.id)
   end
 
-  def student_assignement_status(assignment)
-    all_students.joins(:student_assignments).where(assignment = assignment("student_assignments.status = true")
+  def students_in_cohort(cohort)
+    all_students.where("students.cohort_id = ?", cohort.id)
+    #Student.joins(cohort: :instructors).where("cohort.id = ?", cohort.id).where("instructors.id = ?", self.id)
+  end 
+
+  def cohort_finished_assignment(cohort)
+    #students_in_cohort(cohort).joins(:student_assignments).where("student_assignments.status = true")
   end 
 end
