e7f592edf4baa19075876951b63b7eb63498a400
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-15 11:26:07 -0400

    Add dropdown menu for cohort show page to select existing assignment

--- app/controllers/student_assignments_controller.rb
+++ app/controllers/student_assignments_controller.rb
@@ -5,7 +5,7 @@ class StudentAssignmentsController < ApplicationController
     @student = Student.find(params[:id])
     @assignment = Assignment.find(params[:assignment][:assignment_id])
     @student_assignment = StudentAssignment.where(student_id: @student.id).where(assignment_id: @assignment.id)
-    @student_assignment.first.status = true
+    @student_assignment.first.status = !@student_assignment.first.status
     @student_assignment.first.save
     redirect_to student_path(@student)
   end
