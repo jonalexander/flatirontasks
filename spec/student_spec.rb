35e19332127480c19f7cd614135b4e9aa495d843
laushinka <laurie.malau@gmail.com>
2016-07-15 14:59:17 -0400

    Add #assignment_status desc

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -7,6 +7,17 @@ RSpec.describe Student, type: :model do
   let(:assignment) {Assignment.create!(name: 'Rails Lab', description: 'jdfjdsfjnsdfjn', time: 3, priority: 2)}
   let(:student_assignment) {StudentAssignment.create!(student: student, assignment: assignment, status: false)}
 
+  def assignment_status(assignment)
+    self.student_assignments.where('student_id = ?', self.id).where("assignment_id = ?", assignment.id)[0][:status]
+  end
+
+  describe "#assignment_status" do
+    it "can check an assignment's status"
+    clark = Student.create!(name: 'Clark', email: 'clark@flatiron-school.com', password: '12346', cohort_id: 1)
+    rails = Assignment.create!(name: 'Rails Lab', description: 'jdfjdsfjnsdfjn', time: 3, priority: 2)
+    expect(clark.assignment_status(rails).to eq)
+  end
+
   describe "show page", type: :feature do
     it "has a dashboard" do
       clark = Student.create!(name: 'Clark', email: 'clark@flatiron-school.com', password: '12346', cohort_id: 1)
