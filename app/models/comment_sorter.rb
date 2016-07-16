aa358ae5dcad9a5046cd7e46982e3ac5dfceb146
Jonathan Alexander <jonathandalexander@gmail.com>
2016-07-16 19:58:24 -0400

    Add partials for student#show

--- app/models/comment_sorter.rb
+++ app/models/comment_sorter.rb
@@ -1,12 +1,8 @@
 class CommentSorter < ApplicationRecord
   attr_accessor :object
 
-  #cs = CommentSorter.new
-  #cs.sort_by_most_comments(@assignments)
-
-
   def sort_by_most_comments(object)
-    object.sort_by { |object| object.comments.count }
+    object.sort_by { |object| object.comments.count }.reverse!
   end
 
   def self.top_five_most_comments
