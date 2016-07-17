ff68829cf9367ed1838282b642687eef5cc0638a
laushinka <laurie.malau@gmail.com>
2016-07-17 03:09:59 -0400

    Fix #create

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,27 +1,27 @@
 class InstructorsController < ApplicationController
-  before_action :login_required, :instructor? 
+  before_action :login_required, :instructor?
   skip_before_action :login_required, only: [:new, :create]
   skip_before_action :instructor?, only: [:new, :create]
   #validates_presence_of :name, :email, :password, :password_confirmation
   #validates_uniqueness_of :email
 
-  def index 
+  def index
     @instructors = Instructor.all
-  end 
+  end
 
-  def new 
+  def new
     @instructor = Instructor.new
     @cohorts = Cohort.all
-  end 
+  end
 
-  def create 
-    @instructor = Instructor.(instructor_params)
+  def create
+    @instructor = Instructor.create(instructor_params)
     @cohort = Cohort.find(cohort_param[:id])
     @instructor.cohorts << @cohort
     @instructor.save
     session[:instructor_id] = @instructor.id
     redirect_to instructor_path(@instructor)
-  end 
+  end
 
   def show
     @instructor = Instructor.find(params[:id])
@@ -44,7 +44,7 @@ class InstructorsController < ApplicationController
     @instructor.destroy
   end
 
-  private 
+  private
 
   def instructor_params
     params.require(:instructor).permit(:name, :email, :password, :password_digest)
